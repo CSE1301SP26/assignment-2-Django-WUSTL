2eaa4f64cbf3455c12bbee64d447a266e240bd7b
Django-WUSTL <django@wustl.edu>
2026-02-01 10:39:24 -0600

    Modest improvement; code no longer forces restart if numberOfSticks is not greater than zero.

--- src/assignment2/Nim.java
+++ src/assignment2/Nim.java
@@ -4,14 +4,18 @@ public class Nim {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         int roundNumber = 1;
+        int numberOfSticks = 0;
+        while (numberOfSticks <= 0){
         System.out.println("Let us play a game of Nim! How many sticks are we playing with today?");
-        int numberOfSticks = in.nextInt();
-        if (numberOfSticks <= 0){
-            System.out.println("That simply won't do. Please run this program again.");
-        } else while (numberOfSticks > 0){ 
+        numberOfSticks = in.nextInt();
+            if (numberOfSticks <= 0){
+                System.out.println("We can't play a game with " + numberOfSticks + " sticks! Try again.");
+            }
+        }
+        while (numberOfSticks > 0){ 
             boolean humansTurn = true;
             if (humansTurn){ //Human's turn
-                System.out.println("Round " + roundNumber +". There are " + numberOfSticks + " stick(s) remaining.");
+                System.out.println("Round " + roundNumber +". There's " + numberOfSticks + " stick(s) remaining.");
                 System.out.println("You're up! How many sticks will you take: one, or two?");
                 int sticksTakenByHuman = in.nextInt();
                 if (sticksTakenByHuman == 1 || sticksTakenByHuman == 2){
@@ -30,7 +34,7 @@ public class Nim {
                     //If I try to take 4 sticks, and this code gets tripped, I would never get a chance to reinput a number unless this gets cleared.
                 } 
             if (!humansTurn){ //Computer's turn
-                System.out.println("Round " + roundNumber +". There are " + numberOfSticks + " stick(s) remaining.");
+                System.out.println("Round " + roundNumber +". There's " + numberOfSticks + " stick(s) remaining.");
                 if (numberOfSticks == 1){ //This is not strategy. If there is only one stick remaining, there is only one valid move.
                     numberOfSticks--;
                     System.out.println("The computer takes 1 stick.");
